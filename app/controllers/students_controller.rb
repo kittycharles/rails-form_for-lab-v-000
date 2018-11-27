dbc16e7e528eea3911b5c74340b3b7731b56fb6e
Richard Sanchez <richard.charles.s@gmail.com>
2018-11-27 21:11:10 +0000

    automatically backed up by learn

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,36 @@
+class studentsController < ApplicationController
+
+  def index
+  		@students = Student.all
+  	end
+
+  	def show
+  		@student = Student.find(params[:id])
+  	end
+
+  	def new
+  		@student = Student.new
+  	end
+
+  	def create
+  		@student = Student.new(student_params(:title, :description))
+  		@student.save
+  	  redirect_to student_path(@student)
+  	end
+
+  	def update
+  	  @student = Student.find(params[:id])
+  		@student.update(student_params(:title))
+  		redirect_to student_path(@student)
+  	end
+
+  	def edit
+  	  @student = Student.find(params[:id])
+  	end
+
+  	private
+
+  		def student_params(*args)
+  			params.require(:student).permit(*args)
+  		end
+end
