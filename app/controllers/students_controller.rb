d5dbd4abb074ec10f9a0454ed4783064d9fe2000
Richard Sanchez <richard.charles.s@gmail.com>
2018-11-27 21:11:58 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,14 +13,14 @@ class studentsController < ApplicationController
   	end
 
   	def create
-  		@student = Student.new(student_params(:title, :description))
+  		@student = Student.new(student_params(:first_name, :last_name))
   		@student.save
   	  redirect_to student_path(@student)
   	end
 
   	def update
   	  @student = Student.find(params[:id])
-  		@student.update(student_params(:title))
+  		@student.update(:first_name, :last_name))
   		redirect_to student_path(@student)
   	end
 
